0a51798fbe9d5ac9dd3c5dea957c83cef82c6d43
BatenaAvo24 <132613538+BatenaAvo24@users.noreply.github.com>
2024-05-05 19:18:01 +0100

    getSalaDestinoComOrigem(String salaOrigem) adicionado ao corredor. Vai ser usado para validar o primeiro movimento valido vindo do Mongo.

--- src/main/java/MongoDB/entities/Corredor.java
+++ src/main/java/MongoDB/entities/Corredor.java
@@ -9,4 +9,8 @@ import lombok.*;
 public class Corredor {
     private String salaOrigem;
     private String salaDestino;
+
+    public String getSalaDestinoComOrigem(String salaOrigem){
+        return salaDestino;
+    }
 }
